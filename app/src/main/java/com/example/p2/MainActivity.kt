7fa7d3dd4e0e2d430531b5a8d2cfa35118a6ea31
CesarAguilar1203 <cesaraguilar.2302082@gmail.com>
2025-07-23 07:31:24 -0600

    Add simple login interface

--- app/src/main/java/com/example/p2/MainActivity.kt
+++ app/src/main/java/com/example/p2/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.p2.ui.login.LoginScreen
 import com.example.p2.ui.theme.P2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    LoginScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginPreview() {
     P2Theme {
-        Greeting("Android")
+        LoginScreen()
     }
 }
